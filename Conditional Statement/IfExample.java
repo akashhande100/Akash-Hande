655a1737f96626995ae21d13d3a9101451373e78
akashhande100 <94887703+akashhande100@users.noreply.github.com>
2021-12-06 16:10:49 +0530

    Create IfExample.java

---
+++ Conditional Statement/IfExample.java
@@ -0,0 +1,28 @@
+package conditionalStatement;
+
+public class IfExample {
+	
+	public static void main(String[] arg) {
+		
+		int a = 32;
+		int b = 90;
+		
+		if (a < b) {
+			System.out.println("b is greater than a");
+			}
+		
+		char c = 'a';
+		char d = 'a';
+		
+		if (c==d) {
+			System.out.println("Character c is equal to d");
+			}
+		
+		int e = 321;
+		
+		if(e%2 != 0) {
+			System.out.println("e is odd number");
+		}
+	}
+
+}
