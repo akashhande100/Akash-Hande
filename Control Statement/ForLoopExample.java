03f9d17e2221c5b80beacd7f2fe765498739e6ad
akashhande100 <94887703+akashhande100@users.noreply.github.com>
2021-12-06 16:26:18 +0530

    Create ForLoopExample.java

---
+++ Control Statement/ForLoopExample.java
@@ -0,0 +1,21 @@
+package controlStatement;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+
+		int[] a;
+		a = new int[4];
+
+		a[0] = 10;
+		a[1] = 20;
+		a[2] = 30;
+		a[3] = 40;
+
+		for (int i = 0; i < a.length; i++) {
+
+			System.out.println(a[i]);
+		}
+
+	}
+}
